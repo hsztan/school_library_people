cb0eb635f64ee4aaee4fadc91dfc0edb1b3e6e9e
Henry Nawrocki <hdev@sztanski.com>
2022-06-21 12:34:51 -0500

    Setup store instance to populate with reader class

--- reader.rb
+++ reader.rb
@@ -1,8 +1,14 @@
+require './book'
+require './teacher'
+require './student'
+require './rental'
+
 class Reader
-  def initialize
+  def initialize(store)
     @people_file = File.open('people.json', 'r')
     @books_file = File.open('books.json', 'r')
     @rentals_file = File.open('rentals.json', 'r')
+    @store = store
   end
 
   def read
