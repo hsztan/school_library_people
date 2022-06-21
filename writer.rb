174c4062fb780410054e9f5e4dca2567e5606423
Henry Nawrocki <hdev@sztanski.com>
2022-06-21 11:39:44 -0500

    Preserve People data

--- writer.rb
+++ writer.rb
@@ -3,9 +3,9 @@ require 'json'
 class Writer
   def initialize()
     @people_file = if File.exist?('people.json')
-                    File.open('people.json', 'a')
+                     File.open('people.json', 'a')
                    else
-                    File.open('people.json', 'w')
+                     File.open('people.json', 'w')
                    end
     @books_file = File.open('books.json', 'w')
     @rentals_file = File.open('rentals.json', 'w')
@@ -13,7 +13,7 @@ class Writer
 
   def people(age:, name:, classroom: 'n/a', parent_permission: true, specialization: 'n/a')
     object = [age, classroom, name, parent_permission, specialization]
-    @people_file.write(JSON.generate(object))
+    @people_file.write("#{JSON.generate(object)}\n")
   end
 
   def close_all
